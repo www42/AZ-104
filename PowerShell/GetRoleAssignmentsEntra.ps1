e01fda5cf97371294aac2d55e76c57160537c006
www42 <tjaekel0815@gmail.com>
2023-12-05 12:36:00 +0100

    Add ps1 roles

---
+++ PowerShell/GetRoleAssignmentsEntra.ps1
@@ -0,0 +1,21 @@
+# Entra ID built in roles
+Get-AzureADMSRoleDefinition | Sort-Object DisplayName | Format-Table DisplayName,Description
+Get-AzureADMSRoleDefinition | Measure-Object
+
+# Permissions associated with a role
+$role = Get-AzureADMSRoleDefinition -Filter "displayName eq 'Application Administrator'"
+$role.RolePermissions.AllowedResourceActions
+
+# Which users have the role "Global Administrator"
+$role = Get-AzureADMSRoleDefinition -Filter "displayName eq 'Global Administrator'"
+$roleAssignments = Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$($role.Id)'"
+$roleAssignments
+
+# Translate PrincipalId --> User Display Name
+$roleAssignments | ForEach-Object {
+    try {$userDisplayName = Get-AzureADUser -ObjectId $_.PrincipalId | % DisplayName}
+    catch {$userDisplayName = '<not a user>'}
+    $roleDisplayName = Get-AzureADMSRoleDefinition -Id $_.RoleDefinitionId | % DisplayName
+    "$roleDisplayName $userDisplayName"
+}
+
