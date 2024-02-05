967c3cfaf1824d96050349cd1085648005443cc4
www42 <tjaekel0815@gmail.com>
2024-02-05 17:08:44 +0100

    Add docs

--- PowerShell/SetUserAccessAdministrator.ps1
+++ PowerShell/SetUserAccessAdministrator.ps1
@@ -1,3 +1,7 @@
+# Elevate access to manage all Azure subscriptions and management groups
+
+# https://learn.microsoft.com/en-us/azure/role-based-access-control/elevate-access-global-admin
+
 $User = 'Paul@az.training'
 Get-AzRoleDefinition -Name 'User Access Administrator'
 Get-AzRoleAssignment -RoleDefinitionName 'User Access Administrator' | Format-Table Scope,RoleDefinitionName,SignInName
